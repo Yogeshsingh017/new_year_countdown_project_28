d54ae2f21b2eb3d935f63a1abca194bdf28453f3
YOGESH <142924080+Yogeshsingh017@users.noreply.github.com>
2023-10-27 20:21:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,28 @@
-const num1 = Math.ceil(Math.random() * 10);
-const num2 = Math.ceil(Math.random() * 10);
+const dayEl = document.getElementById("day");
+const hourEl = document.getElementById("hour");
+const minuteEl = document.getElementById("minute");
+const secondEl = document.getElementById("second");
 
-const questionEl = document.getElementById("question");
+const newYearTime = new Date("Jan 1, 2024 00:00:00").getTime();
 
-const inputEl = document.getElementById("input");
+updateCountdown();
 
-const formEl = document.getElementById("form");
+function updateCountdown() {
+  const now = new Date().getTime();
+  const gap = newYearTime - now;
 
-const scoreEl = document.getElementById("score");
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-let score = JSON.parse(localStorage.getItem("score"));
-
-if (!score) {
-  score = 0;
-}
-
-scoreEl.innerText = `score: ${score}`;
-
-questionEl.innerText = `What is ${num1} multiply by ${num2}?`;
-
-const correctAns = num1 * num2;
-
-formEl.addEventListener("submit", () => {
-  const userAns = +inputEl.value;
-  if (userAns === correctAns) {
-    score++;
-    updateLocalStorage();
-  } else {
-    score--;
-    updateLocalStorage();
-  }
-});
-
-function updateLocalStorage() {
-  localStorage.setItem("score", JSON.stringify(score));
+  const d = Math.floor(gap / day);
+  const h = Math.floor((gap % day) / hour);
+  const m = Math.floor((gap % hour) / minute);
+  const s = Math.floor((gap % minute) / second);
+  dayEl.innerText = d;
+  hourEl.innerText = h;
+  minuteEl.innerText = m;
+  secondEl.innerText = s;
+  setTimeout(updateCountdown, 1000)
 }
